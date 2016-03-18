d443cfb761a2c78d1b16f744f2e219ed9ea77cdc
Karoline Busse <busse@cs.uni-bonn.de>
2016-03-18 17:38:20 +0100

    deployment preparations

---
+++ observatory/startup.sh
@@ -0,0 +1,4 @@
+python3 manage.py makemigrations;
+python3 manage.py migrate;
+python3 manage.py collectstatic --noinput;
+gunicorn observatory.wsgi:application --log-level=debug --timeout 120 -w $NUM_WORKER -b :7801
