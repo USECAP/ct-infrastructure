9578d0318822d4231cd43f6dc6fb5587edc951fc
Christian Tiefenau <tiefenau@cs.uni-bonn.de>
2016-04-04 12:36:03 +0200

    gunicorn params fixed

--- observatory/startup.sh
+++ observatory/startup.sh
@@ -3,4 +3,4 @@
 python /observatory/manage.py makemigrations;
 python /observatory/manage.py migrate;
 python /observatory/manage.py collectstatic --noinput;
-gunicorn observatory.wsgi:application --log-level=debug --timeout 120 -w 8 -b :7801
+gunicorn ctobservatory.wsgi:application --log-level=debug --timeout 120 -w 8 -b :7801
