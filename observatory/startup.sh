7126c0ec7b0ea46187a325ec2da2297fe83b1277
Christian Tiefenau <tiefenau@cs.uni-bonn.de>
2016-04-05 11:08:30 +0200

    observatory now uses apache2 and django via wsgi

--- observatory/startup.sh
+++ observatory/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-python /observatory/manage.py makemigrations;
-python /observatory/manage.py migrate;
-python /observatory/manage.py collectstatic --noinput;
-gunicorn ctobservatory.wsgi:application --log-level=debug --timeout 120 -w 8 -b :7801
+cd /observatory
+python manage.py makemigrations;
+python manage.py migrate;
+python manage.py collectstatic --noinput;
+
+gunicorn ctobservatory.wsgi:application --log-level=debug --env DJANGO_SETTINGS_MODULE=ctobservatory.settings --timeout 120 -w 8 -b :7801
