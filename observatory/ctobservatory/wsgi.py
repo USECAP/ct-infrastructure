9ae73de7c3190f9c2f8f5fde58fe385b98661445
Christian Tiefenau <tiefenau@cs.uni-bonn.de>
2016-04-05 11:08:30 +0200

    observatory now uses apache2 and django via wsgi

--- observatory/ctobservatory/wsgi.py
+++ observatory/ctobservatory/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctobservatory.settings")
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 
 application = get_wsgi_application()
