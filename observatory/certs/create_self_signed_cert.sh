471b09246a314dd02717a32fedb81effaecac85c
Sven Zemanek <HSZemi@users.noreply.github.com>
2016-05-20 16:04:29 +0200

    added script for creating self-signed testing certificates

---
+++ observatory/certs/create_self_signed_cert.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# generate ca key and chain file
+openssl req -new -x509 -extensions v3_ca -keyout ca.key -out observatory_chain.pem -days 365
+
+# generate key files
+#openssl req -x509 -newkey rsa:4096 -keyout observatory_key.pem -out observatory.pem -days 365
+openssl genrsa -out observatory_key.pem 4096
+
+# generate csr
+openssl req -new -key observatory_key.pem -out certificate.csr
+
+# sign it
+openssl x509 -req -days 365 -in certificate.csr -CA observatory_chain.pem -CAkey ca.key -set_serial 01 -out observatory.pem
+
+#remove csr
+rm certificate.csr
